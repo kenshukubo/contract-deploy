d5fc69106ef1ba0af4a847674118e6e74c733bee
Kenshu Kubo <kenshu@nextread.co.jp>
2022-10-04 12:35:21 +0900

    mint処理

---
+++ scripts/mint.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const Web3 = require("web3");
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const PUBLIC_KEY = process.env.TEST_PUBLIC_KEY;
+const PRIVATE_KEY = process.env.TEST_PRIVATE_KEY;
+// const PROVIDER_URL = "http://localhost:8545";
+const PROVIDER_URL = process.env.PROVIDER_URL;
+
+async function mintNFT() {
+  const web3 = new Web3(PROVIDER_URL);
+  const contract = require("../artifacts/contracts/NFT.sol/NFT.json");
+  const nftContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS);
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest");
+  const tx = {
+    from: PUBLIC_KEY,
+    to: CONTRACT_ADDRESS,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mint(PUBLIC_KEY).encodeABI(),
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      const tx = signedTx.rawTransaction;
+      if (tx !== undefined) {
+        web3.eth.sendSignedTransaction(tx, function (err, hash) {
+          if (!err) {
+            console.log("The hash of your transaction is: ", hash);
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            );
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err);
+    });
+}
+
+mintNFT();
