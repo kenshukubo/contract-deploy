9ec1c93479ebbc78a2686213a4179754f8402feb
Kenshu Kubo <kenshu@nextread.co.jp>
2022-10-07 09:50:50 +0900

    cloudiary 画像アップロード、コントラクトへの接続

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 // import { fileURLToPath } from "url";
 import './App.css';
-// import web3 from 'web3';
+import Web3 from 'web3';
+import axios from 'axios';
+import NFT from './abi/NFT.json';
+import { AbiItem } from 'web3-utils'
 
 function App() {
   // const [images, setImages] = useState<File[]>([]);
@@ -9,27 +12,26 @@ function App() {
   const [metaMaskFlag, setMetaMaskFlag] = useState<boolean>(false);
   const [account, setAccount] = useState(null);
 	// const [errorMessage, setErrorMessage] = useState(null);
+	const [contract, setContract] = useState<any>(null);
+
+  const endpoint = process.env.PROVIDER_URL;
+  const contractAddress = process.env.CONTRACT_ADDRESS;
 
 	useEffect(() => {
 		var tmpFlag = window.ethereum && window.ethereum.isMetaMask;
 		setMetaMaskFlag(tmpFlag);
+    connectWallet();
+    
+    const web3 = new Web3(`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROVIDER_ID}`);
+    const nftContract = new web3.eth.Contract(NFT.abi as AbiItem[], contractAddress)
+    setContract(nftContract);
 	},[]);
 
 	const connectWallet = async () => {
-		// window.ethereum
-		// .request({ method: "eth_requestAccounts" })
-		// .then((result) => {
-		// 	  setAccount(result[0]);
-		// })
-		// .catch((error) => {
-		//   setErrorMessage(error.message);
-		// });
-
     try {
       const acccounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       if (acccounts.length > 0) {
           setAccount(acccounts[0]);
-          console.log(acccounts[0]);
       }
     } catch (err: any) {
       if (err.code === 4001) {
@@ -45,21 +47,44 @@ function App() {
   const handleOnAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files
 		if (!files) return;
-		// setImages([...images, ...e.target.files]);
     setFile(files[0])
 	};
 
   const handleOnSubmit = async (e: React.SyntheticEvent): Promise<void> => {
 		e.preventDefault();
-    if (!file) return 
+    if (!file) return
 
-    connectWallet();
+    const cloudinaryUploadPreset = "voahej67"
+    const cloudName = "dvtpktk39";
 		const formData  = new FormData();
     formData.append("file", file)
-		// images.map((image) => {
-		// 	data.append("images[]", image);
-		// });
-		console.log(formData)
+    formData.append("upload_preset", cloudinaryUploadPreset)
+    const cloudinaryUploadURL = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+    const requestObj = {
+      url: cloudinaryUploadURL,
+      method: "POST",
+      data: formData
+    }
+    try {
+      await axios(requestObj);
+    } catch (err: any) {
+      console.log(err)
+    }
+
+    // const tx = {
+    //   from: account,
+    //   to: contractAddress,
+    //   data: contract.methods.mint(this.uri).encodeABI(),
+    // };
+    // try {
+    //   await window.ethereum.request({
+    //     method: "eth_sendTransaction",
+    //     params: [tx],
+    //   });
+    //   alert("NFT作成成功");
+    // } catch (error) {
+    //   alert("NFT作成失敗");
+    // }
 	};
 
   return (
@@ -68,16 +93,15 @@ function App() {
         <h2>Mint Test</h2>
       </header>
       <form className="App-form" onSubmit={(e) => handleOnSubmit(e)}>
-        {/* <img
-          src={URL.createObjectURL(file)}
+        <img
+          // src={URL.createObjectURL(file)}
           style={{
             width: "24%",
             borderRadius: "20px",
             marginBottom: "40px"
           }}
-        /> */}
+        />
         <input
-          // id={inputId}
           type="file"
           accept="image/*,.png,.jpg,.jpeg,.gif"
           className='App-input'
